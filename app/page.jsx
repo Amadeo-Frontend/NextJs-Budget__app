7e4c3729e414d6caacd2a3c82ed5caeca3dc2811
Amadeo Bon <amadeosph@outlook.com>
2024-12-10 15:41:05 -0300

    `Update Home component to add className to h1 element`

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Olá</h1>
-   </div>
+    <div>
+      <h1 className="text-5xl font-bold text-red-500">Olá</h1>
+    </div>
   );
 }
