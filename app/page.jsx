70d2b3f5672fec347b53158482613ba528506524
Amadeo Bon <amadeosph@outlook.com>
2024-12-11 08:27:47 -0300

    `Refactor globals.css and page.jsx to include new styles and component`

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,7 @@
 "use client";
 
-export default function Home() {
-  return (
-    <header className="flex items-center justify-between">
-      {/* User information */}
-      <div className="flex items-center gap-2">
-        <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
-          <img
-            src="https://placehold.co/100x100"
-            alt="Profile image"
-            className="object-cover w-full h-full"
-          />
-        </div>
-        <small>Hi, Leon</small>
-      </div>
+import Header from "@/components/Header";
 
-      <nav className="flex items-center gap-2">
-        <div>stats</div>
-        <div>logout</div>
-      </nav>
-    </header>
-  );
+export default function Home() {
+  return <Header />;
 }
---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { IoPieChartSharp } from "react-icons/io5";
+
+const Header = () => {
+  return (
+    <header className="container max-w-2xl px-6 py-6 mx-auto">
+      <div className="flex items-center justify-between">
+        {/* User information */}
+        <div className="flex items-center gap-2">
+          <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+            <img
+              src="https://placehold.co/100x100"
+              alt="Profile image"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <small>Hi, Leon</small>
+        </div>
+
+        <nav className="flex items-center gap-2">
+          <div>
+            <IoPieChartSharp className="text-2xl cursor-pointer" />
+          </div>
+          <div>
+            <button className="btn btn-danger">Sign out</button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
