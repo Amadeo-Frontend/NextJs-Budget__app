d3aca750819eb4e5e46b8ec67375c8fab86d5980
Amadeo Bon <amadeosph@outlook.com>
2024-12-12 01:31:38 -0300

    `Update dependencies and add chartjs-2 component`

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 "use client";
-
 import { currencyFormatter } from "@/lib/utils";
+
 import ExpenseCategoryItem from "@/components/ExpenseCategoryItem";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -29,7 +30,7 @@ const DUMMY_DATA = [
   {
     id: 4,
     title: "Movies",
-    color: "#fff",
+    color: "#000",
     total: 800,
   },
   {
@@ -43,15 +44,15 @@ const DUMMY_DATA = [
 export default function Home() {
   return (
     <main className="container max-w-2xl px-6 mx-auto">
-    <section className="py-3">
-      <small className="text-gray-400 text-md">My Balance</small>
-      <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-    </section>
+      <section className="py-3">
+        <small className="text-gray-400 text-md">My Balance</small>
+        <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+      </section>
 
-    <section className="flex items-center gap-2 py-3">
-      <button className="btn btn-primary">+ Expenses</button>
-      <button className="btn btn-primary-outline">+ Income</button>
-    </section>
+      <section className="flex items-center gap-2 py-3">
+        <button className="btn btn-primary">+ Expenses</button>
+        <button className="btn btn-primary-outline">+ Income</button>
+      </section>
 
       {/* Expenses */}
       <section className="py-6">
@@ -69,6 +70,26 @@ export default function Home() {
         </div>
       </section>
 
-  </main>
+      {/* Chart Section */}
+      <section className="py-6">
+        <h3 className="text-2xl">Stats</h3>
+        <div className="w-1/2 mx-auto">
+          <Doughnut
+            data={{
+              labels: DUMMY_DATA.map((expense) => expense.title),
+              datasets: [
+                {
+                  label: "Expenses",
+                  data: DUMMY_DATA.map((expense) => expense.total),
+                  backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                  borderColor: ["#1645c5"],
+                  borderWidth: 5,
+                },
+              ],
+            }}
+          />
+        </div>
+      </section>
+    </main>
   );
 }
