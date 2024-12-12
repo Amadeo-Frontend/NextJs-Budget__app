4a5e1f2cbd1574845652ad936307a9e3ce0bb774
Amadeo Bon <amadeosph@outlook.com>
2024-12-12 00:06:08 -0300

    `feat: updated page.jsx to include chart.js and dummy data, updated package.json and package-lock.json to reflect new dependencies`

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,57 @@
 "use client";
 
+import { currencyFormatter } from "@/lib/utils";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: "#000",
+    total: 500,
+  },
+  {
+    id: 2,
+    title: "Gass",
+    color: "#009",
+    total: 200,
+  },
+  {
+    id: 3,
+    title: "Fuel",
+    color: "#000",
+    total: 1200,
+  },
+  {
+    id: 4,
+    title: "Movies",
+    color: "#000",
+    total: 800,
+  },
+  {
+    id: 5,
+    title: "Holiday",
+    color: "#000",
+    total: 2000,
+  },
+];
+
 export default function Home() {
   return (
-    <div>
-      <h1>ola teste</h1>
-    </div>
+    <main className="container max-w-2xl px-6 mx-auto">
+    <section className="py-3">
+      <small className="text-gray-400 text-md">My Balance</small>
+      <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+    </section>
+
+    <section className="flex items-center gap-2 py-3">
+      <button className="btn btn-primary">+ Expenses</button>
+      <button className="btn btn-primary-outline">+ Income</button>
+    </section>
+
+  </main>
   );
 }
